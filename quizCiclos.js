bf6f02cbcd4bc084a313311e5743c7333c245830
joquiro369 <joquiro.369@gmail.com>
2021-02-22 09:37:47 -0500

    Ejercicio 1

--- quizCiclos.js
+++ quizCiclos.js
@@ -1,7 +1,9 @@
-function myFunction() {
-    document.getElementById("quiz1").innerHTML = "Gracias por su atencion" ;
-}
 
+var number = 0;
+var numero = prompt("digite un valor menor a 5")
+while (number <= 5) {
+    console.log(number); number < 5;
+}
 
 
 
